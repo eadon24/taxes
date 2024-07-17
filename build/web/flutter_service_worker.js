26cbbdc6ab6b4f8de617115b7c52841cd3245388
eadon24 <exchangeeadon@gmail.com>
2024-07-17 12:10:27 -0500

    12meridum

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "bda69b2d94259a62c8374769b8373c49
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "76f0ad7b79b8b36a54d2332aae4c9339",
+"flutter_bootstrap.js": "ca49b527ff0fbb66d7b3287113293f5d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "fc32727c8712d3dc6e789a9014987ce1",
-"/": "fc32727c8712d3dc6e789a9014987ce1",
-"main.dart.js": "4e30d60dd890bcb45492cbcb31aeeb6e",
+"index.html": "3dd1b3ecfe9a7a1fd54937a655a00b72",
+"/": "3dd1b3ecfe9a7a1fd54937a655a00b72",
+"main.dart.js": "7cda16d325553cf814e9319526367066",
 "manifest.json": "68fdeec0cd1b18f0d320918acc644083",
 "version.json": "879d7927a67cfa78327bb0cd362b8234"};
 // The application shell files that are downloaded before a service worker can
--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Actualización: 17/07/2024',
+                  'Actualización: 17/07/2024 12:00pm',
                   style: TextStyle(color: Colors.red),
                 ),
                 SizedBox(height: 20),
@@ -118,11 +118,11 @@ class _ExchangeCalculatorState extends State<ExchangeCalculator> {
   // Variables que debes modificar para actualizar las tasas de cambio
   double exchangeRate = 10.21; // Tasa de cambio soles a bolívares
   double dollarParallelRate =
-      40.98; // Tasa de cambio bolívares a dólares paralelo
+      40.92; // Tasa de cambio bolívares a dólares paralelo
   double dollarBCVRate = 36.51; // Tasa de cambio bolívares a dólares BCV
 
   // Variable que debes modificar para actualizar la fecha y hora
-  String updateText = 'Actualización: 17/07/2024';
+  String updateText = 'Actualización: 17/07/2024 12:00 pm';
 
   void _updateFields({String source = ''}) {
     double soles = double.tryParse(_solesController.text) ?? 0.0;
