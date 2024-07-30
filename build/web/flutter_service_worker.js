3876d201e28f72164df52604147060f09f7d4d72
eadon24 <exchangeeadon@gmail.com>
2024-07-30 13:09:41 -0500

    30 julio tarde

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "bda69b2d94259a62c8374769b8373c49
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "d531351c3fda8ef270f79ad6124ad683",
+"flutter_bootstrap.js": "71b488a8e7e9a0b634a995d59381a74e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7744190bb7ded67af2360ad18a6fa0ff",
-"/": "7744190bb7ded67af2360ad18a6fa0ff",
-"main.dart.js": "4ef6a04fa564376492de9522e4e59905",
+"index.html": "dadfbd92e766110caabfb3c158579f6a",
+"/": "dadfbd92e766110caabfb3c158579f6a",
+"main.dart.js": "fe4a624ed6f44b6a34774008f2986232",
 "manifest.json": "68fdeec0cd1b18f0d320918acc644083",
 "version.json": "879d7927a67cfa78327bb0cd362b8234"};
 // The application shell files that are downloaded before a service worker can
--- lib/main.dart
+++ lib/main.dart
@@ -118,7 +118,7 @@ class _ExchangeCalculatorState extends State<ExchangeCalculator> {
   // Variables que debes modificar para actualizar las tasas de cambio
   double exchangeRate = 10.30; // Tasa de cambio soles a bolívares
   double dollarParallelRate =
-      42.86; // Tasa de cambio bolívares a dólares paralelo
+      42.35; // Tasa de cambio bolívares a dólares paralelo
   double dollarBCVRate = 36.61; // Tasa de cambio bolívares a dólares BCV
 
   // Variable que debes modificar para actualizar la fecha y hora
