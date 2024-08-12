e3ba63efcbc786b94e04d89c4bb2af30e06e7795
eadon24 <exchangeeadon@gmail.com>
2024-08-12 12:17:38 -0500

    12 agosto tarde

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,14 +24,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "bda69b2d94259a62c8374769b8373c49
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "56cb0947a5a6b8b3bcf857a2232ae2d0",
+"flutter_bootstrap.js": "5b3ddeafa1e3b7415e02b2953de3ae48",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c3dd64a2b9b8bd9469d74eea4c047332",
-"/": "c3dd64a2b9b8bd9469d74eea4c047332",
-"main.dart.js": "8e21e1db501061f6083b98118796ad28",
+"index.html": "fb3a21150eb91169a6f3d6293f0be910",
+"/": "fb3a21150eb91169a6f3d6293f0be910",
+"main.dart.js": "86a17f9d2e6c4ef29c8168e473261301",
 "manifest.json": "68fdeec0cd1b18f0d320918acc644083",
 "version.json": "879d7927a67cfa78327bb0cd362b8234"};
 // The application shell files that are downloaded before a service worker can
