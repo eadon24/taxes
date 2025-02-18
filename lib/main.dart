7824bdf9550c8c35798aff71b0bb8a1e61eebdc9
eadon24 <exchangeeadon@gmail.com>
2025-02-18 13:23:47 -0500

    1405

--- lib/main.dart
+++ lib/main.dart
@@ -122,7 +122,7 @@ class _ExchangeCalculatorState extends State<ExchangeCalculator> {
   // Variables que debes modificar para actualizar las tasas de cambio
   double exchangeRate = 20.05; // Tasa de cambio soles a bolívares
   double dollarParallelRate =
-      78.30; // Tasa de cambio bolívares a dólares paralelo
+      79.51; // Tasa de cambio bolívares a dólares paralelo
   double dollarBCVRate = 62.10; // Tasa de cambio bolívares a dólares BCV
 
   // Variable que debes modificar para actualizar la fecha y hora
