46f4a71f57a0daefc3b6e6ee0677064548b3f68f
eadon24 <exchangeeadon@gmail.com>
2025-04-26 17:02:02 -0500

    26

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Actualización 24-04-2025',
+                  'Actualización 26-04-2025',
                   style: TextStyle(color: Colors.red),
                 ),
                 SizedBox(height: 20),
@@ -112,14 +112,14 @@ class _ExchangeCalculatorState extends State<ExchangeCalculator> {
   final TextEditingController _dollarBCVController = TextEditingController();
 
   // Variables que debes modificar para actualizar las tasas de cambio
-  double exchangeRate = 26.30; // Tasa de cambio soles a bolívares
+  double exchangeRate = 26.60; // Tasa de cambio soles a bolívares
   double dollarParallelRate =
-      105.34; // Tasa de cambio bolívares a dólares paralelo
-  double dollarBCVRate = 83.42; // Tasa de cambio bolívares a dólares BCV
+      104.96; // Tasa de cambio bolívares a dólares paralelo
+  double dollarBCVRate = 84.42; // Tasa de cambio bolívares a dólares BCV
 
   // Variable que debes modificar para actualiza
   //r la fecha y hora
-  String updateText = 'Actualización 24-04-2025';
+  String updateText = 'Actualización 26-04-2025';
 
   void _updateFields({String source = ''}) {
     double soles = double.tryParse(_solesController.text) ?? 0.0;
