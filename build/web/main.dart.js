34c89c44ef4cc00287da5946fabd15a06aa0cacd
eadon24 <exchangeeadon@gmail.com>
2025-05-21 09:41:11 -0500

    26

--- build/web/main.dart.js
+++ build/web/main.dart.js
@@ -71647,14 +71647,14 @@ o.c=r
 q=A.wi(p.r.a.a)
 if(q==null)q=0
 o.d=q
-if(a==="soles"){s=o.b=n*31.4
+if(a==="soles"){s=o.b=n*33.4
 o.c=s/131.46
-o.d=s/94.86}else if(a==="bolivares"){o.a=s/31.4
+o.d=s/94.86}else if(a==="bolivares"){o.a=s/33.4
 o.c=s/131.46
 o.d=s/94.86}else if(a==="dollarParallel"){s=o.b=r*131.46
-o.a=s/31.4
+o.a=s/33.4
 o.d=s/94.86}else if(a==="dollarBCV"){s=o.b=q*94.86
-o.a=s/31.4
+o.a=s/33.4
 o.c=s/131.46}p.ae(new A.a8g(o,p,a))},
 tq(a){A.oD(new A.jU(a))
 this.c.ap(t.Pu).f.F6(B.Kx)},
@@ -71664,10 +71664,10 @@ s.e.ji(B.h6)
 s.f.ji(B.h6)
 s.r.ji(B.h6)},
 Y5(){var s=this
-A.oD(new A.jU("      Calculadora EADON\n      Actualizaci\xf3n 21-05-2025\n      Cantidad en Soles  "+s.d.a.a+" - Tasa 31.4 Bs.\n      Cantidad en Bs. a Recibir: "+s.e.a.a+"\n      D\xf3lares (Paralelo) "+s.f.a.a+" - tasa 131.46 Bs. \n      D\xf3lares (BCV) "+s.r.a.a+" - tasa 94.86 Bs.  \n    "))
+A.oD(new A.jU("      Calculadora EADON\n      Actualizaci\xf3n 21-05-2025\n      Cantidad en Soles  "+s.d.a.a+" - Tasa 33.4 Bs.\n      Cantidad en Bs. a Recibir: "+s.e.a.a+"\n      D\xf3lares (Paralelo) "+s.f.a.a+" - tasa 131.46 Bs. \n      D\xf3lares (BCV) "+s.r.a.a+" - tasa 94.86 Bs.  \n    "))
 s.c.ap(t.Pu).f.F6(B.Ky)},
 N(a){var s=this,r=null
-return A.Sg(A.c([A.a4P(s.d,A.EY(r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,B.cq,"Tasa 31.4 Bs -  Soles a Enviar ",!0,r,r,r,r,r,r,r,r,A.p8(r,r,B.fl,r,r,new A.a8h(s),r,r,r),r,r,r,r),B.h7,new A.a8i(s)),A.a4P(s.e,A.EY(r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,B.cq,"Cantidad en Bs. a Recibir ",!0,r,r,r,r,r,r,r,r,A.p8(r,r,B.fl,r,r,new A.a8j(s),r,r,r),r,r,r,r),B.h7,new A.a8k(s)),A.a4P(s.f,A.EY(r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,B.cq,"D\xf3lar (Paralelo) - 131.46 Bs",!0,r,r,r,r,r,r,r,r,A.p8(r,r,B.fl,r,r,new A.a8l(s),r,r,r),r,r,r,r),B.h7,new A.a8m(s)),A.a4P(s.r,A.EY(r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,B.cq,"D\xf3lar (BCV) - 94.86 Bs",!0,r,r,r,r,r,r,r,r,A.p8(r,r,B.fl,r,r,new A.a8n(s),r,r,r),r,r,r,r),B.h7,new A.a8o(s)),B.el,A.ajq(B.PA,s.ga50(),A.ajr(r,r,B.zB,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r)),B.Kn,A.ajq(B.PC,s.gY4(),A.ajr(r,r,B.zs,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r))],t.E),B.cK,B.bi,B.ch)}}
+return A.Sg(A.c([A.a4P(s.d,A.EY(r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,B.cq,"Tasa 33.4 Bs -  Soles a Enviar ",!0,r,r,r,r,r,r,r,r,A.p8(r,r,B.fl,r,r,new A.a8h(s),r,r,r),r,r,r,r),B.h7,new A.a8i(s)),A.a4P(s.e,A.EY(r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,B.cq,"Cantidad en Bs. a Recibir ",!0,r,r,r,r,r,r,r,r,A.p8(r,r,B.fl,r,r,new A.a8j(s),r,r,r),r,r,r,r),B.h7,new A.a8k(s)),A.a4P(s.f,A.EY(r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,B.cq,"D\xf3lar (Paralelo) - 131.46 Bs",!0,r,r,r,r,r,r,r,r,A.p8(r,r,B.fl,r,r,new A.a8l(s),r,r,r),r,r,r,r),B.h7,new A.a8m(s)),A.a4P(s.r,A.EY(r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,B.cq,"D\xf3lar (BCV) - 94.86 Bs",!0,r,r,r,r,r,r,r,r,A.p8(r,r,B.fl,r,r,new A.a8n(s),r,r,r),r,r,r,r),B.h7,new A.a8o(s)),B.el,A.ajq(B.PA,s.ga50(),A.ajr(r,r,B.zB,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r)),B.Kn,A.ajq(B.PC,s.gY4(),A.ajr(r,r,B.zs,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r,r))],t.E),B.cK,B.bi,B.ch)}}
 A.a8g.prototype={
 $0(){var s=this,r=s.c
 if(r!=="soles")s.b.d.seV(B.c.a1(s.a.a,2))
--- lib/main.dart
+++ lib/main.dart
@@ -116,7 +116,7 @@ class _ExchangeCalculatorState extends State<ExchangeCalculator> {
   final TextEditingController _dollarBCVController = TextEditingController();
 
   // Variables que debes modificar para actualizar las tasas de cambio
-  double exchangeRate = 31.40; // Tasa de cambio soles a bolívares
+  double exchangeRate = 33.40; // Tasa de cambio soles a bolívares
   double dollarParallelRate =
       131.46; // Tasa de cambio bolívares a dólares paralelo
   double dollarBCVRate = 94.86; // Tasa de cambio bolívares a dólares BCV
