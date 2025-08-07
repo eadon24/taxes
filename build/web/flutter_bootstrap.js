f94e46f64fe446c44bdbc85ea20cdecc75bf49f8
eadon24 <exchangeeadon@gmail.com>
2025-08-07 09:32:53 -0500

    15

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2200958772"
+    serviceWorkerVersion: "946878339"
   }
 });
