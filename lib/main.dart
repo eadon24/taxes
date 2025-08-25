8caaf44c2ac5ac1487a44520305ff74a2d3f74e1
eadon24 <exchangeeadon@gmail.com>
2025-08-25 10:05:31 -0500

    15

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Actualización 24-08-2025',
+                  'Actualización 25-08-2025',
                   style: TextStyle(
                     color: Color.fromARGB(255, 8, 8, 8),
                     fontWeight: FontWeight.bold,
@@ -107,7 +107,7 @@ class _ExchangeCalculatorState extends State<ExchangeCalculator> {
   final TextEditingController _dollarBCVController = TextEditingController();
 
   final List<Map<String, dynamic>> tasasPorPais = [
-    {'pais': 'Perú', 'tasa': 56.50, 'modo': 'multiplicar', 'bandera': '🇵🇪'},
+    {'pais': 'Perú', 'tasa': 58.50, 'modo': 'multiplicar', 'bandera': '🇵🇪'},
     {'pais': 'Chile', 'tasa': 0.203, 'modo': 'multiplicar', 'bandera': '🇨🇱'},
     {'pais': 'Colombia', 'tasa': 21.00, 'modo': 'dividir', 'bandera': '🇨🇴'},
     {'pais': 'EE.UU.', 'tasa': 192.0, 'modo': 'multiplicar', 'bandera': '🇺🇸'},
@@ -115,7 +115,7 @@ class _ExchangeCalculatorState extends State<ExchangeCalculator> {
 
   double dollarBCVRate = 141.88;
   int selectedIndex = 0;
-  String updateText = 'Actualización 24-08-2025';
+  String updateText = 'Actualización 25-08-2025';
 
   void _updateFields({String source = ''}) {
     double soles = double.tryParse(_solesController.text) ?? 0.0;
