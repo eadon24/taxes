d506ecaf34c5152fee301e720e3523fa136a04eb
eadon24 <exchangeeadon@gmail.com>
2025-12-24 08:34:48 -0500

    16

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1706053856"
+    serviceWorkerVersion: "4169915409"
   }
 });
--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Actualización 22-12-2025',
+                  'Actualización 24-12-2025',
                   style: TextStyle(
                     color: Color.fromARGB(255, 8, 8, 8),
                     fontWeight: FontWeight.bold,
@@ -113,9 +113,9 @@ class _ExchangeCalculatorState extends State<ExchangeCalculator> {
     {'pais': 'EE.UU.', 'tasa': 405.0, 'modo': 'multiplicar', 'bandera': '🇺🇸'},
   ];
 
-  double dollarBCVRate = 285.4024;
+  double dollarBCVRate = 291.3524;
   int selectedIndex = 0;
-  String updateText = 'Actualización 22-12-2025';
+  String updateText = 'Actualización 24-12-2025';
 
   void _updateFields({String source = ''}) {
     double soles = double.tryParse(_solesController.text) ?? 0.0;
