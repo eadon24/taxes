91b1b7e5dbec1533e38b8b7018291247e0ebc6b5
eadon24 <exchangeeadon@gmail.com>
2026-04-01 11:08:36 -0500

    22

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Actualización 28-03-2026',
+                  'Actualización 01-04-2026',
                   style: TextStyle(
                     color: Color.fromARGB(255, 8, 8, 8),
                     fontWeight: FontWeight.bold,
@@ -115,7 +115,7 @@ class _ExchangeCalculatorState extends State<ExchangeCalculator> {
 
   double dollarBCVRate = 471.70;
   int selectedIndex = 0;
-  String updateText = 'Actualización 28-03-2026';
+  String updateText = 'Actualización 01-04-2026';
 
   void _updateFields({String source = ''}) {
     double soles = double.tryParse(_solesController.text) ?? 0.0;
