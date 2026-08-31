4fd58d6cf22a188f8ce49a1f52037312a7b5e767
eadon24 <exchangeeadon@gmail.com>
2026-08-31 14:15:31 -0500

    31/08/2026 02

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bda69b2d94259a62c8374769b8373c49
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "7d6fbd838cf3842b0d400ecb1bdcbed8",
+"flutter_bootstrap.js": "23b9a30379f1e1b60c967de832ae2f01",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bda69b2d94259a62c8374769b8373c49
 "/": "9699e3ca445af53b4466b9f2df83248f",
 "main.dart.js": "1e141d27dbf981b5ad911697f5c4814d",
 "manifest.json": "68fdeec0cd1b18f0d320918acc644083",
-"tasas.json": "b7a4a1e6dd98faa5b51b41c9f402ff66",
+"tasas.json": "f57cb02fb9a31ac2fc70bae6e3cd1898",
 "version.json": "879d7927a67cfa78327bb0cd362b8234"};
 // The application shell files that are downloaded before a service worker can
 // start.
